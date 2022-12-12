414f32c9f2d59c96331188d1402f5b9fdf180d4d
Julius Djudjaku <juliusdjudjaku@gmail.com>
2022-12-12 16:07:31 +0100

    created navbar and footer + some pages

---
+++ client/js/components/footerComponent.js
@@ -0,0 +1,15 @@
+const footer = document.getElementById('footer')
+
+footer.innerHTML = `
+<div class="contact">
+<h3>Kontakt</h3>
+<p>profile@email.com</p>
+<p>Tel: 010-0000000</p>
+</div>
+<div class="location">
+<h3>Vi finns här</h3>
+<address>Superbanan Gatan 1337</address>
+<address>Göteborg 504 54</address>
+
+</div>
+`
