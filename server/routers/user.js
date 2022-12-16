38bbbfd316a309a3000052a1e20f94ea8d1fe8d7
fredriklexo <91253151+fredriklexo@users.noreply.github.com>
2022-12-16 10:44:46 +0100

    Endpoints for skills

--- server/routers/user.js
+++ server/routers/user.js
@@ -29,6 +29,90 @@ router.get("/getAll", async (req, res) => {
     }
   });
 
+// ...
+router.get("/getSkills/css", async (req, res) => {
+    
+  
+  try {
 
+    const user = await User.find().sort( { css: -1 } )
+    
+    if(user[0].css){
+
+      res.json(user);
+    }else{
+      res.json("Det finns tyvärr ingen som är ledig just nu..")
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// ...
+router.get("/getSkills/html", async (req, res) => {
+    
+  
+  try {
+
+    const user = await User.find().sort( { html: -1 } )
+    if(user[0].html){
+
+      res.json(user);
+    }else{
+      res.json("Det finns tyvärr ingen som är ledig just nu..")
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// ...
+router.get("/getSkills/js", async (req, res) => {
+    
+  
+  try {
+
+    const user = await User.find().sort( { javaScript: -1 } )
+    if(user[0].javaScript){
+
+      res.json(user);
+    }else{
+      res.json("Det finns tyvärr ingen som är ledig just nu..")
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+// ...
+router.get("/getSkills/php", async (req, res) => {
+    
+  
+  try {
+
+    const user = await User.find().sort( { php: -1 } )
+    if(user[0].php){
+
+      res.json(user);
+    }else{
+      res.json("Det finns tyvärr ingen som är ledig just nu..")
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+router.get("/getSlug/:value", async (req, res) => {
+    
+  
+    try {
+    
+      const user = await User.find({slug: req.params.value})
+     
+      res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
   
 export default router
